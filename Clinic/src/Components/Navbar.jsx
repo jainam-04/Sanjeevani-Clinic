5c0dc946c24f0f0686c0b6951f938c89af793b93
Jainam <jainamrupani04@gmail.com>
2024-08-11 13:08:53 +0530

    navbar made sticky

--- Clinic/src/Components/Navbar.jsx
+++ Clinic/src/Components/Navbar.jsx
@@ -58,7 +58,7 @@ function Navbar() {
   );
 
   return (
-    <nav className="bg-white shadow-md">
+    <nav className="bg-white shadow-md sticky top-0 left-0 right-0">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex items-center">
